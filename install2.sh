0f611e197cd8c84dd987b99a84e09787af3d0da8
TheBACKTRACK <83034169+TheBACKTRACK@users.noreply.github.com>
2022-02-07 21:01:07 +0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -5,8 +5,6 @@ echo Installing postmarketOS UI...
 apk update
 apk add -u postmarketos-ui-xfce4 lightdm lightdm-gtk-greeter xfce4-terminal neofetch
 useradd -m -g wheel user
-touch /home/user/.bashrc
-echo neofetch > /home/user/.bashrc
 echo Set user password after installation completed
 rc-update add lightdm
 curl https://raw.githubusercontent.com/postmarketOS/artwork/master/logo/logo.png -o /home/user/Desktop
