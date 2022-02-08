555f719fab0e31ae745289112b2539a79ec4c856
TheBACKTRACK <83034169+TheBACKTRACK@users.noreply.github.com>
2022-02-08 15:50:53 +0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -8,4 +8,10 @@ useradd -m -g wheel user
 echo Set user password after installation completed
 rc-update add lightdm
 curl https://raw.githubusercontent.com/postmarketOS/artwork/master/logo/logo.png -o /home/user/Desktop
+
+curl https://raw.githubusercontent.com/postmarketOS/artwork/master/wallpapers/logo/logo_dark.jpg -o /home/user/Desktop
+curl https://raw.githubusercontent.com/postmarketOS/artwork/master/wallpapers/logo/pmos-grass-square.jpg -o /home/user/Desktop
+curl https://raw.githubusercontent.com/postmarketOS/artwork/master/wallpapers/logo/pmos-grass.jpg -o /home/user/Desktop
+curl https://raw.githubusercontent.com/postmarketOS/artwork/master/wallpapers/logo/pmos.jpg -o /home/user/Desktop
+curl https://raw.githubusercontent.com/postmarketOS/artwork/master/wallpapers/logo/postmarketos.jpg -o /home/user/Desktop
 echo Installation completed!
